c4c08f02f68d9cc9a4e8e3894f34ffc10731dcc8
Thomas Buida <thomasbuida@gmail.com>
2016-03-01 15:23:26 -0600

    Added more spells

--- javascripts/spells.js
+++ javascripts/spells.js
@@ -31,3 +31,25 @@ Gauntlet.SpellBook.Sphere = function() {
   this.type = this.damageTypes[random];
 };
 Gauntlet.SpellBook.Sphere.prototype = new Gauntlet.SpellBook.Spell();
+
+
+
+Gauntlet.SpellBook.Bolt = function() {
+  this.name = "bolt";
+  this.damage = Math.floor(Math.random() * 8 + 12);
+
+  var random = Math.round(Math.random() * (this.damageTypes.length - 1));
+  this.type = this.damageTypes[random];
+};
+Gauntlet.SpellBook.Bolt.prototype = new Gauntlet.SpellBook.Spell();
+
+
+Gauntlet.SpellBook.Wand = function() {
+  this.name = "wand";
+  this.damage = Math.floor(Math.random() * 6 + 18);
+
+  var random = Math.round(Math.random() * (this.damageTypes.length - 1));
+  this.type = this.damageTypes[random];
+};
+Gauntlet.SpellBook.Wand.prototype = new Gauntlet.SpellBook.Spell();
+
